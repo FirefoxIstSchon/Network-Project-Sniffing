9c18d8e64a8dcc9340f1678eceadd32958783d9d
Berke <berkeagababaoglu@gmail.com>
2018-11-15 15:02:41 +0300

    follower run deleted

--- src/Follower.java
+++ src/Follower.java
@@ -11,9 +11,36 @@ public class Follower {
     BufferedReader reader;
     PrintWriter writer;
 
-    static String SERVER_ADDRESS;
-    static int SERVER_PORT;
+    static String SERVER_ADDRESS = "172.20.40.159";
+    static int SERVER_PORT = 4444;
+    static Follower follower;
 
+
+    public static void main(String[] args){
+
+        // create a follower instance
+
+        follower = new Follower(SERVER_ADDRESS, SERVER_PORT);
+
+        follower.initialize_connection();
+
+        if (follower.socket == null) {
+
+            System.out.println("Follower : connectivity is not established.");
+
+        } else {
+
+            System.out.println("Follower : connectivity is established.");
+
+            // update with master
+
+            new Thread(new Master_Connecter(follower, 10)).start();
+
+            // follower.terminate_connection();
+
+        }
+
+    }
 
     public Follower(String SERVER_ADDRESS, int SERVER_PORT){
         this.SERVER_ADDRESS=SERVER_ADDRESS;
