e7d71ce314898544b4991f46e2ce01070fd4a19e
K <31030673+FirefoxIstSchon@users.noreply.github.com>
2018-11-15 13:56:36 +0300

    working on master

--- src/Master.java
+++ src/Master.java
@@ -1,4 +1,52 @@
-package PACKAGE_NAME;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Master {
+
+    static Master master;
+
+    static Map<String, String> database;
+
+    public Master(){
+
+        // todo : load from disk here.
+        // if not found.
+        database = new HashMap<>();
+
+        // also save to disk when done.
+
+    }
+
+    public static Master get_instance(){
+        if (master == null) master = new Master();
+        return master;
+    }
+
+    public static String client_put(String key, String value){
+
+        String msg = "";
+
+        if (database.get(key) == null){
+
+            database.put(key, value);
+            msg = "Put successful: Key" ;
+
+        } else {
+
+            msg = "Put unsuccessful: Key " + key + " already exists.";
+
+        }
+
+
+        return msg;
+    }
+
+    public static String client_get(String key){
+
+        String msg = "";
+
+
+        return msg;
+    }
+
 }
--- src/Master_run.java
+++ src/Master_run.java
@@ -1,4 +1,2 @@
-package PACKAGE_NAME;
-
 public class Master_run {
 }
