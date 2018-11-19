3e2fc23fbb939191c59bd056dacebaca6bc07289
K <31030673+FirefoxIstSchon@users.noreply.github.com>
2018-11-19 20:14:16 +0300

    push

--- src/Sniffer.java
+++ src/Sniffer.java
@@ -1,9 +1,66 @@
-
+import java.util.Scanner;
 
 public class Sniffer {
+
+    static String hexData = "";
+
     public static void main(String[] args){
 
+        String input = "";
+        Scanner keyboard = new Scanner(System.in);
+
+        String line;
+        while (keyboard.hasNextLine()) {
+            line = keyboard.nextLine();
+            if (line.isEmpty()) {
+                break;
+            }
+            input += " "+line ;
+        }
+
+        System.out.println(input);
 
 
+        hexData = eraseSpaces(input);
+        System.out.println(hexData);
+
+
+        findIP(hexData);
+        findPort(hexData);
+
+    }
+
+    public static void findIP(String hexData){
+
+        String sourceIP =hexData.substring(54,62);
+        System.out.println("Source IP: ");
+        String hex="";
+        int len =sourceIP.length();
+        for(int i =0; i< len; i++)
+        {
+            hex=hex+sourceIP.charAt(i);
+            System.out.print(Integer.parseInt(hex,16));
+            if(i!=len){
+                System.out.print(".");
+            }
+        }
+
+    }
+
+    public static void findPort(String hexData) {
+
+
+
+    }
+
+    public static String eraseSpaces(String s){
+
+        String[] temp = s.split("\n");
+        String[] line;
+
+
+
+
+        return "";
     }
 }
